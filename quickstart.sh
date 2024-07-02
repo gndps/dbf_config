ba85ba5ed6b1aa32fd5cca547eb2a9abae8681bf
Gundeep Singh <gundeep.singh@dialpad.com>
2024-07-02 01:46:01 -0700

    add mackup

--- quickstart.sh
+++ quickstart.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 # install
 bash <(curl -s https://raw.githubusercontent.com/gndps/dot_bash_files/main/install.sh)
-source $HOME/.bash_profile
 
 # config
-bfcli config https://github.com/gndps/dbf_config.git
+$HOME/.bash_files/bfcli config https://github.com/gndps/dbf_config.git
 
 # initialize default profile
-bfcli init default
+$HOME/.bash_files/bfcli init default
